190fdd5e5fead3b80bbb5a3ec22935b0797b95de
NOTB727\lay <abdoullaj.fall@scaiconsulting.gruppo-scai.it>
2018-01-28 21:55:22 +0100

    website almost complete

--- everGreen/js/scripting.js
+++ everGreen/js/scripting.js
@@ -1,7 +1,15 @@
 
 var $body = $('body');
+var $nav = $('.navBarBlocked');
 var $buttonMenu = $('.menu-button');
+var $dropFirst = $('.dropDownButton');
+var $dropSecond = $('.dropSecondInside')
+var $sideButton = $('.rightSideBar');
+
 var MENU = 'menuOpened';
+var MOVEMENU = 'menuMoved';
+var DROP = 'fristDropped';
+var DROP2 = 'secondDropped';
 
 var _manageClass = function(classToggle) {
     $body.toggleClass(classToggle);
@@ -9,4 +17,17 @@ var _manageClass = function(classToggle) {
 
 $buttonMenu.on('click', function() {
   _manageClass(MENU);
+});
+
+$sideButton.on('click', function (){
+    _manageClass(MOVEMENU);
+});
+
+$dropFirst.on('click', function (){
+    _manageClass(DROP);
+});
+
+
+$dropSecond.on('click', function (){
+    _manageClass(DROP2);
 });
